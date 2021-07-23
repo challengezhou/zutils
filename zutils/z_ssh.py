908fdf188cb5b91fa59499911eaa669453443ccd
zhoujian <zhoujian@cloudatum.com>
2021-07-23 11:02:05 +0800

    bug fix custom user

--- zutils/z_ssh.py
+++ zutils/z_ssh.py
@@ -9,15 +9,17 @@ _homed_path = os.path.expanduser(default_dir)
 
 
 @click.command('s')
-@click.option('-a', '--add', is_flag=True, help='Add a new ip relevance,cover exist')
+@click.option('-i', '--ip', help='Add a new ip relevance')
+@click.option('-s', '--spec-ip', help='Specify the short ip or update existed ip relevance')
+@click.option('--des', help='Add description to ip')
 @click.option('-d', '--delete', is_flag=True, help='Delete a ip relevance')
-@click.option('-u', '--upload-key', is_flag=True, help='Use ssh-copy-id to upload ssh pub key, option -a enhanced')
-@click.option('-l', '--ls', is_flag=True, help='List all ip (short_ip  ip  user  port)')
-@click.option('-m', '--migrate', help='migrate db to add field')
+@click.option('-k', '--upload-key', is_flag=True, help='Use ssh-copy-id to upload ssh pub key, option -i enhanced')
+@click.option('-l', '--ls', is_flag=True, help='List all ip')
+@click.option('--migrate', help='migrate db to add field， only for dev')
+@click.option('-u', '--user', help='User to connect')
+@click.option('-p', '--port', help='Port to connect')
 @click.argument('short_ip', default='', type=str)
-@click.argument('user', default='', type=str)
-@click.argument('port', default='', type=str)
-def ssh_conn(add, delete, upload_key, ls, migrate, short_ip, user, port):
+def ssh_conn(ip, spec_ip, des, delete, upload_key, ls, migrate, short_ip, user, port):
     '''Utility for ssh connect,use a short ip related to actual_ip'''
     db = _get_db()
     if not db:
@@ -31,12 +33,12 @@ def ssh_conn(add, delete, upload_key, ls, migrate, short_ip, user, port):
     elif delete:
         del db[short_ip]
         _save_db(db)
-    elif add:
-        _add_ip(upload_key, short_ip, db, user, port)
+    elif ip:
+        _add_ip(db, upload_key, ip, user, port, des, spec_ip)
     elif upload_key:
-        _upload_key(port, user, short_ip)
+        _upload_key(ip, user, port)
     else:
-        _conn_ip(short_ip, db, user, port)
+        _conn_ip(db, short_ip, user, port)
 
 
 def _db_add_field(db, field):
@@ -48,27 +50,34 @@ def _db_add_field(db, field):
 
 
 def _list_ip(db):
-    record_format = '{:<10s}{:<20s}{:<20s}{:<6s}'
-    click.echo(record_format.format('short', 'ip', 'user', 'port'))
+    record_format = '{:<10s}{:<20s}{:<20s}{:<10s}{:<20s}'
+    click.echo(record_format.format('short', 'ip', 'user', 'port', 'des'))
     for key in db:
-        click.echo(record_format.format(key, db[key]['ip'], db[key]['user'], str(db[key]['port'] if db[key]['port'] else '22')))
+        click.echo(record_format.format(key, db[key]['ip'], db[key]['user'], str(db[key]['port'] if db[key]['port'] else '22'), 
+        db[key]['des'] if db[key]['des'] else ''))
 
 
-def _add_ip(upload_key, ip, db, user, port):
-    ip_suffix = ip.split('.')[-1]
+def _add_ip(db, upload_key, ip, user, port, des, spec_ip):
+    if spec_ip:
+        short_ip = spec_ip
+    else:
+        short_ip = ip.split('.')[-1]
+        if db[short_ip]:
+            click.echo("short_ip [" + short_ip + '] exist!')
     p = _take_port(port)
-    db[ip_suffix] = {'ip': ip, 'user': user, 'port': p}
+
+    db[short_ip] = {'ip': ip, 'user': user, 'port': p, 'des': des}
     _save_db(db)
     if upload_key:
-        _upload_key(p, user, ip)
+        _upload_key(ip, user, p)
 
 
-def _upload_key(port, user, ip):
+def _upload_key(ip, user, port):
     click.echo('Upload ssh pub key to ' + ip + ',user:' + user)
     os.system('ssh-copy-id -p %s %s@%s' % (port, user, ip))
 
 
-def _conn_ip(short_ip, db, user, port):
+def _conn_ip(db, short_ip, user, port):
     if len(db.keys()) == 0:
         click.echo('DB no any ip,use -a option to add')
     elif not short_ip:
@@ -79,14 +88,11 @@ def _conn_ip(short_ip, db, user, port):
             click.echo('No any ip related to %s' % short_ip)
         else:
             actual_ip = target['ip']
+            # no pass user param
             if not user:
-                user = target.get('user', 'root')
-                if not user:
-                    user = 'root'
+                user = _take_user(target.get('user'))
             if not port:
-                port = target.get('port', '22')
-                if not port:
-                    port = '22'
+                port = _take_port(target.get('port'))
             click.echo(
                 'Attempt to conn [ \33[32m%s@%s %s\033[0m ]' % (user, actual_ip, port))
             os.system('ssh -p %s %s@%s' % (port, user, actual_ip))
@@ -111,6 +117,12 @@ def _take_port(port):
     if not port:
         return '22'
     return port
+
+
+def _take_user(user):
+    if not user:
+        return 'root'
+    return user
 
 
 def _save_db(db):
