f76fc2e45a01b0414f7a239737629f47c09314e4
zhoujian <***>
2023-02-27 15:30:02 +0800

    修改参数结构

--- zutils/z_ssh.py
+++ zutils/z_ssh.py
@@ -9,18 +9,19 @@ _homed_path = os.path.expanduser(default_dir)
 
 
 @click.command('s')
-@click.option('-i', '--ip', help='Add a new ip relevance')
-@click.option('-s', '--spec-ip', help='Specify the short ip or update existed ip relevance')
-@click.option('--des', help='Add description to ip')
-@click.option('-d', '--delete', is_flag=True, help='Delete a ip relevance')
-@click.option('-k', '--upload-key', is_flag=True, help='Use ssh-copy-id to upload ssh pub key, option -i enhanced')
-@click.option('-l', '--ls', is_flag=True, help='List all ip')
-@click.option('--migrate', help='migrate db to add field， only for dev')
-@click.option('-u', '--user', help='User to connect')
+@click.option('-s', '--short', help='Specify the shortcut for host ssh login')
+@click.option('-h', '--host', help='Host to connect')
 @click.option('-p', '--port', help='Port to connect')
-@click.argument('short_ip', default='', type=str)
-def ssh_conn(ip, spec_ip, des, delete, upload_key, ls, migrate, short_ip, user, port):
-    '''Utility for ssh connect,use a short ip related to actual_ip'''
+@click.option('-u', '--user', help='User to connect')
+@click.option('-a', '--add',  is_flag=True, help='Add or update a new shortcut')
+@click.option('-d', '--delete', is_flag=True, help='Delete by shortcut')
+@click.option('--des', help='Add description to shortcut')
+@click.option('--upload-key', is_flag=True, help='Use ssh-copy-id to upload ssh pub key, option -a enhanced')
+@click.option('-l', '--ls', is_flag=True, help='List all shortcut')
+@click.option('--migrate', help='migrate db to add field, only for dev')
+@click.argument('shortcut', default='', type=str)
+def ssh_conn(host, short, des, add, delete, upload_key, ls, migrate, shortcut, user, port):
+    '''Utility for ssh connect,use a shortcut login actual_host'''
     db = _get_db()
     if not db:
         db = {}
@@ -28,17 +29,22 @@ def ssh_conn(ip, spec_ip, des, delete, upload_key, ls, migrate, short_ip, user, 
     if migrate:
         _db_add_field(db, migrate)
         _save_db(db)
+        return
     elif ls:
         _list_ip(db)
-    elif delete:
-        del db[short_ip]
+        return
+    if delete:
+        del db[short]
         _save_db(db)
-    elif ip:
-        _add_ip(db, upload_key, ip, user if user else 'root', port, des, spec_ip)
-    elif upload_key:
-        _upload_key(ip, user, port)
+        return
+    if add:
+        if host:
+            _add_ip(db, upload_key, host, user if user else 'root', port, des, short)
+        else:
+            click.echo('must provide host -h/--host')
+        return
     else:
-        _conn_ip(db, short_ip, user, port)
+        _conn_ip(db, shortcut, user, port)
 
 
 def _db_add_field(db, field):
@@ -51,51 +57,53 @@ def _db_add_field(db, field):
 
 def _list_ip(db):
     record_format = '{:<10s}{:<20s}{:<20s}{:<10s}{:<20s}'
-    click.echo(record_format.format('short', 'ip', 'user', 'port', 'des'))
+    click.echo(record_format.format('shortcut', 'host', 'user', 'port', 'des'))
     for key in db:
-        click.echo(record_format.format(key, db[key]['ip'], db[key]['user'], str(db[key]['port'] if db[key]['port'] else '22'), 
+        click.echo(record_format.format(key, db[key]['host'], db[key]['user'], str(db[key]['port'] if db[key]['port'] else '22'), 
         db[key]['des'] if db[key]['des'] else ''))
 
 
-def _add_ip(db, upload_key, ip, user, port, des, spec_ip):
-    if spec_ip:
-        short_ip = spec_ip
+def _add_ip(db, upload_key, host, user, port, des, spec_shortcut):
+    if spec_shortcut:
+        shortcut = spec_shortcut
     else:
-        short_ip = ip.split('.')[-1]
-        if short_ip in db:
-            click.echo("short_ip [" + short_ip + '] exist!')
+        shortcut = host.split('.')[-1]
+        if shortcut in db:
+            click.echo("shortcut [" + shortcut + '] exist!')
+            return
     p = _take_port(port)
 
-    db[short_ip] = {'ip': ip, 'user': user, 'port': p, 'des': des}
+    db[shortcut] = {'host': host, 'user': user, 'port': p, 'des': des}
     _save_db(db)
     if upload_key:
-        _upload_key(ip, user, p)
+        _upload_key(host, user, p)
 
 
-def _upload_key(ip, user, port):
-    click.echo('Upload ssh pub key to ' + ip + ',user:' + user)
-    os.system('ssh-copy-id -p %s %s@%s' % (port, user, ip))
+def _upload_key(host, user, port):
+    click.echo('Upload ssh pub key to ' + host + ',user:' + user)
+    os.system('ssh-copy-id -p %s %s@%s' % (port, user, host))
 
 
-def _conn_ip(db, short_ip, user, port):
+def _conn_ip(db, shortcut, user, port):
+    if not shortcut:
+        click.echo('shortcut must be gaven')
+        return
     if len(db.keys()) == 0:
-        click.echo('DB no any ip,use -a option to add')
-    elif not short_ip:
-        click.echo('Short_ip must be gaven')
+        click.echo('DB no any shortcut,use -a option to add')
+        return
+    target = db.get(shortcut)
+    if not target:
+        click.echo('shortcut %s not found ' % shortcut)
     else:
-        target = db.get(short_ip)
-        if not target:
-            click.echo('No any ip related to %s' % short_ip)
-        else:
-            actual_ip = target['ip']
-            # no pass user param
-            if not user:
-                user = _take_user(target.get('user'))
-            if not port:
-                port = _take_port(target.get('port'))
-            click.echo(
-                'Attempt to conn [ \33[32m%s@%s %s\033[0m ]' % (user, actual_ip, port))
-            os.system('ssh -p %s %s@%s' % (port, user, actual_ip))
+        actual_host = target['host']
+        # no pass user param
+        if not user:
+            user = _take_user(target.get('user'))
+        if not port:
+            port = _take_port(target.get('port'))
+        click.echo(
+            'Attempt to conn [ \33[32m%s@%s %s\033[0m ]' % (user, actual_host, port))
+        os.system('ssh -p %s %s@%s' % (port, user, actual_host))
 
 
 def _get_db():
